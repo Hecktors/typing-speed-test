0846fae04e4da5daaee0723bf0330e0da8732fd9
hecktors <tohe.webdev@gmail.com>
2021-04-12 12:23:20 +0200

    Finish basic logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import "./App.css"
 
 export default function App() {
   const DEFAULT_TIME = 5
 
-  const [text, setText] = useState("")
+  const [isFirstRender, setIsFirstRender] = useState(true)
   const [timeRemaining, setTimeRemaining] = useState(DEFAULT_TIME)
   const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [numWords, setNumWord] = useState(0)
+  const textAreaRef = useRef(null)
+
+  function countWords(text) {
+    const words = text.trim().split(" ")
+    return words.filter((word) => word !== "").length
+  }
+
+  function startTimer() {
+    setIsTimeRunning(true)
+    setNumWord(0)
+    textAreaRef.current.value = ""
+    textAreaRef.current.disabled = false
+    textAreaRef.current.focus()
+  }
+
+  function stopTimer() {
+    setTimeRemaining(DEFAULT_TIME)
+    setIsTimeRunning(false)
+    setNumWord(countWords(textAreaRef.current.value))
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setIsFirstRender(false)
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1)
+      }, 1000)
+    } else if (timeRemaining === 0) {
+      stopTimer()
+    }
+  }, [timeRemaining, isTimeRunning])
 
   return (
     <div className="App">
@@ -16,11 +48,13 @@ export default function App() {
       </header>
       <main>
         <div className="textBoxContainer">
-          <textarea rows="10" cols="50" />
+          <textarea ref={textAreaRef} disabled={!isTimeRunning} />
           <div className="counter">{timeRemaining}</div>
         </div>
-        <button>Start</button>
-        <p>Word count: 0</p>
+        <button onClick={startTimer} disabled={isTimeRunning}>
+          Start
+        </button>
+        <p>{!isFirstRender && !isTimeRunning ? "Word count: " + numWords : ""}</p>
       </main>
     </div>
   )
